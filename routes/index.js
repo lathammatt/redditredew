d05877fa6fc0e6da043da2347516bec02bedb38c
Mike Meadows <mmeadow33@gmail.com>
2016-09-15 11:48:23 -0500

    rendering pug files

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const { Router } = require("Express");
+const router = Router();
+
+
+router.get("/", (req, res)=>{
+  res.render("index")
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const express = require('express');
 
 const app = express()
 const port = 3000
+const routes = require("./routes/")
+
 
 app.set('view engine', 'pug');
+app.use(routes)
+app.use(express.static("public"))
+
+
 
 const server = app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
