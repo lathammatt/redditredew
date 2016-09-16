d6f52eaa582d03a8dc8f1f6fd352ea2701a1eb4f
Mike Meadows <mmeadow33@gmail.com>
2016-09-16 15:16:24 -0500

    added heroku stuff

--- database.js
+++ database.js
@@ -2,10 +2,11 @@
 
 const mongoose = require('mongoose')
 
-const MONGODB_URL = 'mongodb://localhost:27017/reddit'
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/reddit'
 /////directing to local db
 
 
 mongoose.Promise = Promise
 
 module.exports.connect = () => mongoose.connect(MONGODB_URL)
+module.exports.disconnect = () => mongoose.disconnect()
